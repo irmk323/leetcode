f28cc5e3fe1183c205ee58fdeebd519f22fb8ba4
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-02 12:25:58 +0100

    Update 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java

--- Java/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ Java/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -1,21 +1,23 @@
 class Solution {
-public TreeNode buildTree(int[] preorder, int[] inorder) {
-    if(preorder.length!=inorder.length) return null;
-    return build(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
-}
-
-public TreeNode build(int [] preorder, int preLow, int preHigh, int[] inorder, int inLow, int inHigh){
-    if(preLow>preHigh || inLow>inHigh) return null;
-    TreeNode root = new TreeNode(preorder[preLow]);
-   
-    int inorderRoot = inLow;
-    for(int i=inLow;i<=inHigh;i++){
-        if(inorder[i]==root.val){ inorderRoot=i; break; }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length -1 ;
+        if(preorder.length!=inorder.length) return null;
+        return buildTree_rec(preorder, 0 , preorder.length -1  , inorder , 0, inorder.length -1);
     }
-   
-    int leftTreeLen = inorderRoot-inLow;
-    root.left = build(preorder, preLow+1, preLow+leftTreeLen, inorder, inLow, inorderRoot-1);
-    root.right = build(preorder, preLow+leftTreeLen+1, preHigh, inorder, inorderRoot+1, preHigh);       
-    return root;        
-}
+    
+    public TreeNode buildTree_rec(int[] preorder, int p1 , int p2, int[] inorder , int i1 , int i2) {
+        if(p1 > p2 || i1 > i2) return null;
+        TreeNode root = new TreeNode(preorder[p1]);
+        
+        int it = i1;
+        for(int i = i1; i <= i2; i++){
+            if(inorder[i]==root.val){ it = i; break;  }
+
+        }
+        int diff = it - i1;
+        root.left = buildTree_rec(preorder, p1 + 1, p1 + diff, inorder,i1, it - 1);
+        root.right = buildTree_rec(preorder, p1 + diff +1, p2 , inorder, it+1 , i2);
+        return root;
+        
+    }
 }
