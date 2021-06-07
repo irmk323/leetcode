6fcef19c6d78e9d68d49c7d376b3eb8360db084f
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-07 22:32:04 +0100

    Update 117_Populating_Next_Right_Pointers_in_Each_Node_II.java

--- 117_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ 117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -20,3 +20,28 @@ class Node {
     }
 };
 */
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null)  return null;
+        
+        Node head = root;
+        while (head != null){
+            Node dummy = new Node(0);
+            Node temp = dummy;
+            while(head != null){
+                if(head.left !=null){
+                    temp.next= head.left;
+                    temp = temp.next;
+                }
+                if(head.right !=null){
+                    temp.next= head.right;
+                    temp = temp.next;
+                }
+                head= head.next;
+            }
+            head = dummy.next;
+        }
+        return root;
+    }
+}
