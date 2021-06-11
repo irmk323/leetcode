980093907beffdc69a499db2798b168abfc33c18
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-11 23:15:48 +0100

    Update 297_Serialize_and_Deserialize_Binary_Tree.java

--- Java/297_Serialize_and_Deserialize_Binary_Tree.java
+++ Java/297_Serialize_and_Deserialize_Binary_Tree.java
@@ -1,1 +1,41 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    
+   public void selialize(TreeNode root, ArrayList<Integer> array){
+        if(root == null){
+            array.add(-1);
+            return;
+        }
+        array.add(-1);
+        selialize(root.left, array);
+        selialize(root.right, array);
+        return;
+    }
+    
+    int index = 0;
+    public TreeNode deselialize(ArrayList<Integer> array){
+        if(index == array.size() ||  array.get(index) == -1){
+            index += 1;
+            return null;
+        }
+        TreeNode root = new TreeNode(array.get(index));
+        index += 1;
+        root.left = deselialize(array);
+        root.right = deselialize(array);
+        return root;
+    }    
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
