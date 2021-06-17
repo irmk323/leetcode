387c88c759d7e4697ebe615c639462cc6582bcef
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-17 23:41:10 +0100

    Update 24_Swap_Nodes_in_Pairs.java

--- Java/24_Swap_Nodes_in_Pairs.java
+++ Java/24_Swap_Nodes_in_Pairs.java
@@ -15,13 +15,14 @@ class Solution {
         ListNode current = temp;
         
         while(current.next != null && current.next.next != null){
-            ListNode first_node = current.next;
-            ListNode second_node = current.next.next;
-            first_node.next = second_node.next;
-            current.next = second_node;
-            current.next.next = first_node;
-            current = current.next.next;
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current  = current.next.next;
         }
         return temp.next;
+        
     }
 }
