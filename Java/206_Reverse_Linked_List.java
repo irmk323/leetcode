5ca5a6348337aab2b832959ed58972dc03dd59da
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-18 23:55:26 +0100

    Update 206_Reverse_Linked_List.java

--- Java/206_Reverse_Linked_List.java
+++ Java/206_Reverse_Linked_List.java
@@ -23,3 +23,23 @@ class Solution {
         return prev;
     }
 }
+
+// retcursive
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverse(head, null);
+    }
+    public ListNode reverse(ListNode head , ListNode newHead) {
+        if(head == null){
+        return newHead;
+        } 
+        
+        ListNode next  = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+        return  reverse(head, newHead);
+    }
+    
+}
+
