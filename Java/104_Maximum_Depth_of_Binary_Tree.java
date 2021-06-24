581a9692826d87467f32cda67bf5613fa85d2ac7
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-24 19:44:17 +0100

    Update 104_Maximum_Depth_of_Binary_Tree.java

--- Java/104_Maximum_Depth_of_Binary_Tree.java
+++ Java/104_Maximum_Depth_of_Binary_Tree.java
@@ -19,3 +19,29 @@ class Solution {
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
+
+//iterative 
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        LinkedList<TreeNode> stack = new LinkedList();
+        LinkedList<Integer> depths = new LinkedList();
+        stack.add(root);
+        depths.add(1);
+        
+        int depth = 0;
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pollLast();
+            int currDepth = depths.pollLast();
+            if (curr != null) {
+            depth = Math.max(depth, currDepth);
+            stack.add(curr.right);
+            depths.add(currDepth + 1);
+             stack.add(curr.left);
+            depths.add(currDepth + 1);                    
+             }
+        }
+        return depth;
+        
+    }
+}
