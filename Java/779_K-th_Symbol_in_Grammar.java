9328fa4193b509390aa0139cd945d43f09b08d4d
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-06-28 19:45:48 +0100

    Update 779_K-th_Symbol_in_Grammar.java

--- Java/779_K-th_Symbol_in_Grammar.java
+++ Java/779_K-th_Symbol_in_Grammar.java
@@ -1,25 +1,12 @@
 class Solution {
     public int kthGrammar(int n, int k) {
         if(n == 1) return 0;
-        //basically ceil(K/2) become double
-        //if k is odd, it is easy to get the parent k position just +1
-        // if k is 3, parent k is 2, (3 + 1) /2 is 2.
         int parent = kthGrammar(n-1, (k+1)/2);
-        boolean isKOdd = true;
-        if(k % 2 == 0) isKOdd = false;
-        
+        boolean isKOdd = k % 2 == 1 ? true :false;
         if(parent == 1){
-            if(isKOdd){
-                return 1;
-            }else{
-                return 0;
-            }
+            return isKOdd ? 1 : 0;
         }else{
-            if(isKOdd){
-                return 0;
-            }else{
-                return 1;
-            }
+            return isKOdd ? 0 : 1;
         }
     }
 }
