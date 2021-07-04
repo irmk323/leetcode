a78fd1bd76cdd46cc0312a88ee77d982f719c51a
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-07-04 23:39:00 +0100

    Update 52_N-Queens_II.java

--- Java/52_N-Queens_II.java
+++ Java/52_N-Queens_II.java
@@ -1,34 +1,32 @@
 class Solution {
-    private int size;
+    int size = 0;
     public int totalNQueens(int n) {
         size = n;
-        return backtrack(0, new HashSet<>(), new HashSet<>(),new HashSet<>());
+        return backTrack(0, new HashSet<>(),new HashSet<>(),new HashSet<>());
     }
-    
-    private int backtrack(int row, Set<Integer> diagonals, 
-                          Set<Integer> antiDiagonals, Set<Integer> cols){
-        if(row == size ) return 1;
+    private int backTrack(int row, Set<Integer> diagno, Set<Integer> antiDiago,  Set<Integer> cols){
+        if (row == size) return 1;
         
         int solutions = 0;
         for(int col = 0; col < size; col++){
-            int currDiagonal = row - col;
-            int currAntiDiagonal = row + col;
+            int currDiagno = row - col;
+            int currAntiDiagno = row + col;
             if(cols.contains(col) ||
-              diagonals.contains(currDiagonal) ||
-               antiDiagonals.contains(currAntiDiagonal)
+               diagno.contains(currDiagno) ||
+               antiDiago.contains(currAntiDiagno)
               ){
                 continue;
-            }    
-        cols.add(col);
-        diagonals.add(currDiagonal);
-        antiDiagonals.add(currAntiDiagonal);
-        
-        solutions += backtrack(row + 1 , diagonals, antiDiagonals, cols);
-        
-        cols.remove(col);
-        diagonals.remove(currDiagonal);
-        antiDiagonals.remove(currAntiDiagonal);
-    }
-    return solutions;
+            }
+               cols.add(col);
+               diagno.add(currDiagno);
+               antiDiago.add(currAntiDiagno);
+            
+            solutions += backTrack(row +1 ,diagno, antiDiago, cols );
+            
+               cols.remove(col);
+               diagno.remove(currDiagno);
+               antiDiago.remove(currAntiDiagno);
+        }
+        return solutions;
     }
 }
