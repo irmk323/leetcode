d093a879c8679e6fb61d7ecf5472af50b1ef97f7
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-07-19 00:20:51 +0100

    Update 22_Generate_Parentheses.java

--- Java/22_Generate_Parentheses.java
+++ Java/22_Generate_Parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<>();
-        backtrack(list, "", 0, 0, n);
+        backtrack(list, "" , 0, 0 , n);
         return list;
     }
     public void backtrack(List<String> list, String str, int open, int close, int max){
@@ -9,9 +9,9 @@ class Solution {
             list.add(str);
             return;
         }
-        if(open < max)
-            backtrack(list, str+"(", open+1, close, max);
+        if(open < max )
+            backtrack(list, str+"(" , open + 1, close, max);
         if(close < open)
-            backtrack(list, str+")", open, close+1, max);
+            backtrack(list, str+")" , open, close+1, max);
     }
 }
