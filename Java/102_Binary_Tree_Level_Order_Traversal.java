81fc17eb7b39729c33a6510c4c0b12be30e4ae0d
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-07-19 17:51:13 +0100

    Update 102_Binary_Tree_Level_Order_Traversal.java

--- Java/102_Binary_Tree_Level_Order_Traversal.java
+++ Java/102_Binary_Tree_Level_Order_Traversal.java
@@ -42,19 +42,18 @@ class Solution {
 
 //DFS version
 class Solution {
-public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        levelHelper(res, root, 0);
-        return res;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        helper(lists, root, 0);
+        return lists;
     }
-    
-    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
-        if (root == null) return;
-        if (height >= res.size()) {
-            res.add(new LinkedList<Integer>());
+    public void helper(List<List<Integer>>lists, TreeNode root , int height){
+        if(root == null) return;
+        if(height == lists.size()){
+            lists.add(new ArrayList<>());
         }
-        res.get(height).add(root.val);
-        levelHelper(res, root.left, height+1);
-        levelHelper(res, root.right, height+1);
+        lists.get(height).add(root.val);
+        helper(lists, root.left, height+1);
+        helper(lists, root.right, height+1);
     }
 }
