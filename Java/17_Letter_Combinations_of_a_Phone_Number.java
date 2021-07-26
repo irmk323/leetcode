3940c7fce87ff53fdc109468ecd711d92187061c
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-07-26 14:32:45 +0100

    Create 17_Letter_Combinations_of_a_Phone_Number.java

---
+++ Java/17_Letter_Combinations_of_a_Phone_Number.java
@@ -0,0 +1,19 @@
+class Solution {
+public List<String> letterCombinations(String digits) {
+		LinkedList<String> ans = new LinkedList<String>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+		for(int i =0; i<digits.length();i++){
+            //getNumericValue数字を意味するchar型をそのままの見た目の数値に変換したい場合
+			int x = Character.getNumericValue(digits.charAt(i));
+            //peekは最初の要素を取得して、削除なし、removeは削除あり、
+			while(ans.peek().length()==i){
+				String t = ans.remove();
+				for(char s : mapping[x].toCharArray())
+					ans.add(t+s);
+			}
+		}
+		return ans;
+	}
+}
