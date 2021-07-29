66321b519df19bb2335d54c4f879052b19c94e03
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-07-29 17:10:38 +0100

    Create 977_Squares_of_a_Sorted_Array.java

---
+++ Java/977_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        int left = 0, right = n -1;
+        for(int i = n-1; i>=0; i--){
+            int square;
+            if(Math.abs(nums[left]) < Math.abs(nums[right]) ){
+                square = nums[right];
+                right--;
+            }else{
+                square = nums[left];
+                left++;
+            }
+            ret[i] = square * square;
+        }
+        return ret;
+    }
+}
