162913f5e5e465a7ffb2d8b35ecb56cb5d25d39e
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-01 23:43:34 +0100

    Update 1089_Duplicate_Zeros.java

--- Java/1089_Duplicate_Zeros.java
+++ Java/1089_Duplicate_Zeros.java
@@ -1,3 +1,27 @@
+
+// 1 ms
+class Solution {
+public void duplicateZeros(int[] arr) {
+        int countZero = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) countZero++;
+        }
+        int len = arr.length + countZero;
+    //新規の配列を作成しない、iはオリジナルの配列でjは新しい場所を示す
+        for (int i = arr.length - 1, j = len - 1; i < j; i--, j--) {
+            if (arr[i] != 0) {
+                if (j < arr.length) arr[j] = arr[i];
+            } else {
+                if (j < arr.length) arr[j] = arr[i];
+                j--;
+                //0の時は2回コピーする
+                if (j < arr.length) arr[j] = arr[i]; 
+            }
+        }
+    }  
+}
+
+// this solution takes 14ms
 class Solution {
 public void duplicateZeros(int[] arr) {
 int n=arr.length;
