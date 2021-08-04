82b7682e698e2616709b22a810a06d943b7cf342
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-04 22:45:34 +0100

    Create 27_Remove_Element.java

---
+++ Java/27_Remove_Element.java
@@ -0,0 +1,15 @@
+// Time complexity : O(n)
+// Space complexity : 0(1)
+//0ms
+class Solution {
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
+}
