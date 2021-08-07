f3a974d59a2f3b260e3e9223f0fa071010074613
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-07 22:24:58 +0100

    Update 941_Valid_Mountain_Array.java

--- Java/941_Valid_Mountain_Array.java
+++ Java/941_Valid_Mountain_Array.java
@@ -1,21 +1,18 @@
 // Time O(n)
 // Space O(1)
 class Solution {
-    public boolean validMountainArray(int[] A) {
-        int N = A.length,  i = 0;
-
-        // walk up
-        while (i+1 < N && A[i] < A[i+1])
-            i++;
-
-        // peak can't be first or last
-        if (i == 0 || i == N-1)
-            return false;
-
+    public boolean validMountainArray(int[] arr) {
+        
+        int N = arr.length, i = 0;
+        //walk up
+        while(i+1 < N && arr[i] < arr[i+1]) i++;
+        
+        //peek cannot be first or end
+        if(i == 0 || i == N-1) return false;
+        
         // walk down
-        while (i+1 < N && A[i] > A[i+1])
-            i++;
-
-        return i == N-1;
+        while(i+1 < N && arr[i] > arr[i+1]) i++;
+        
+        return i == N -1;
     }
 }
