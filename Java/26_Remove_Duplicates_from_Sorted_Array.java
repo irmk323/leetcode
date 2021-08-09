a86079aae83bd34633cb440ccfb0c9b185506133
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-09 20:52:57 +0100

    Update 26_Remove_Duplicates_from_Sorted_Array.java

--- Java/26_Remove_Duplicates_from_Sorted_Array.java
+++ Java/26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,16 +1,16 @@
-//use 2 pointer,
+//use 2 pointer
 //Time complextiy O(n)
 // Space complexity : O(1).
 
 class Solution {
     public int removeDuplicates(int[] nums) {
         int p = 0;
-        for(int j = 1;j<nums.length; j++){
+        for(int j = 1; j<nums.length; j++){
             if(nums[p] != nums[j]){
                 p++;
                 nums[p] = nums[j];
             }
         }
-        return p +1;
+        return p+1;
     }
 }
