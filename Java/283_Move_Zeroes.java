b56830afea668744697344f0e421fbf19c87574d
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-11 20:01:39 +0100

    Update 283_Move_Zeroes.java

--- Java/283_Move_Zeroes.java
+++ Java/283_Move_Zeroes.java
@@ -1,3 +1,25 @@
+// Time:O(1)
+// Space:O(n)
+// Updated 11/Aug
+
+class Solution {
+public void moveZeroes(int[] nums) {
+    if (nums == null || nums.length == 0) return;        
+
+    int insertPos = 0;
+    for (int num: nums) {
+        if (num != 0) nums[insertPos++] = num;
+    }        
+
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+}
+}
+
+
+///=============================
+
 class Solution {
     public void moveZeroes(int[] nums) {
         for(int i = 0, j = 0; i < nums.length && j < nums.length; j++){
