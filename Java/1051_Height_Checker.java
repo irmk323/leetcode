70e704b9a612e01944eaff0d6eaf9f1b8652f57a
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-17 23:44:15 +0100

    Update 1051_Height_Checker.java

--- Java/1051_Height_Checker.java
+++ Java/1051_Height_Checker.java
@@ -1,28 +1,26 @@
 // On the way
-
 class Solution {
     public int heightChecker(int[] heights) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] heightToFreq = new int[101];
         
-        for(int i = 0; i< heights.length; i++){
-            map.put(heights[i], map.getOrDefault(heights[i], 0) + 1);
+        for (int height : heights) {
+            heightToFreq[height]++;
         }
-        
-        System.out.println(map);
         
         int result = 0;
-        List keys = new ArrayList(map.keySet());
+        int curHeight = 0;
         
-        for (int i = 0, curHeight = 0 ; curHeight < heights.length ; curHeight++) {
-            if(map.get((int)keys.get(i)) > 0) {
-                if(heights[curHeight] != (int)keys.get(i)){
-                    result++;
-                }
-                map.put((int)keys.get(i), map.get((int)keys.get(i)) - 1);    
-            }else{
-                i++;
+        for (int i = 0; i < heights.length; i++) {
+            while (heightToFreq[curHeight] == 0) {
+                curHeight++;
             }
+            
+            if (curHeight != heights[i]) {
+                result++;
+            }
+            heightToFreq[curHeight]--;
         }
+        
         return result;
     }
 }
