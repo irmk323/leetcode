2862821579a6945e634bfa6b1347ec2c730b9ec6
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-08-23 22:45:50 +0100

    Update 487_Max_Consecutive_Ones_II.java

--- Java/487_Max_Consecutive_Ones_II.java
+++ Java/487_Max_Consecutive_Ones_II.java
@@ -1,15 +1,15 @@
 //Time: O(n) Space: O(1)
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0, zero = 0, k = 1; // flip at most k zero
-        for (int l = 0, h = 0; h < nums.length; h++) {
-            if (nums[h] == 0)                                           
-                zero++;
-            while (zero > k)
-                if (nums[l++] == 0)
-                    zero--;                                     
-            max = Math.max(max, h - l + 1);
-        }                                                               
-        return max;         
+        int max = 0, zero = 0,  k =1;
+            for(int l = 0, h = 0; h<nums.length; h++){
+               if(nums[h] == 0)
+                   zero++;
+                while(zero > k )
+                    if(nums[l++] == 0)
+                        zero--;
+                max = Math.max(max, h-l+1);
+            }
+        return max;
     }
 }
