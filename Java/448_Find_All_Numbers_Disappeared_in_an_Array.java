b1bd6742c58efa3385cbd8819dfb39fc87bd6123
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-09-11 19:57:56 +0100

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.java

--- Java/448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ Java/448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -1,3 +1,31 @@
+// Time:O(N)
+// Space:O(1)
+
+=========================================================
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        for (int i = 0; i < nums.length; i++) {
+            int newIndex = Math.abs(nums[i]) - 1;
+
+            if (nums[newIndex] > 0) {
+                nums[newIndex] *= -1;
+            }
+        }
+        
+        List<Integer> result = new LinkedList<Integer>();
+        
+        for (int i = 1; i <= nums.length; i++) {
+            
+            if (nums[i - 1] > 0) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
+
 =========================================================
 // Time:O(N)
 // Space:O(N)
