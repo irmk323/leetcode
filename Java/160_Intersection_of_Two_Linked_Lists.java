14584bd1da92bfddfbd09fa844fcde1bfb5fafc2
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-09-21 17:03:39 +0100

    Update 160_Intersection_of_Two_Linked_Lists.java

--- Java/160_Intersection_of_Two_Linked_Lists.java
+++ Java/160_Intersection_of_Two_Linked_Lists.java
@@ -9,17 +9,18 @@
  *     }
  * }
  */
-// Time O(n) 
-// Space O(1)
+
+
+// Time O(n)  Space O(1)
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
         while(a != b){
-            a = a ==null ? headB : a.next;
-            b = b ==null ? headA : b.next;
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
         return a;
-        
     }
 }
