f293147072d8f98c9827f6dee7b18367c110c448
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-09-29 23:36:20 +0100

    Update 707_Design_Linked_List.java

--- Java/707_Design_Linked_List.java
+++ Java/707_Design_Linked_List.java
@@ -1,3 +1,114 @@
+//=============== Double Linked List====================
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode prev;
+    ListNode(int x) { val = x;}
+}
+class MyLinkedList {
+    int size;
+    ListNode head, tail;
+    public MyLinkedList() {
+        size = 0;
+        head = new ListNode(0);
+        tail = new ListNode(0);
+        head.next = tail;
+        tail.prev = head;
+        
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size ) return -1;
+        ListNode curr = head;
+        if(index + 1 < size - index)
+            for(int i = 0; i< index + 1 ; ++i) curr = curr.next;
+        else{
+            curr = tail;
+            for(int i = 0; i< size - index ; ++i) curr = curr.prev;
+        }
+        return curr.val;
+        
+    }
+    
+    public void addAtHead(int val) {
+        ListNode pred = head, succ = head.next;
+        ++size;
+        ListNode toAdd = new ListNode(val);
+        toAdd.prev = pred;
+        toAdd.next = succ;
+        pred.next = toAdd;
+        succ.prev = toAdd;
+        
+    }
+    
+    public void addAtTail(int val) {
+        ListNode succ = tail, pred = tail.prev;
+        ++size;
+        ListNode toAdd = new ListNode(val);
+        toAdd.prev= pred;
+        toAdd.next = succ;
+        pred.next = toAdd;
+        succ.prev = toAdd;
+        
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index > size) return;
+        if(index < 0) index = 0;
+        ListNode pred, succ;
+        if(index < size - index){
+            pred = head;
+            for(int i = 0; i < index; i++) pred = pred.next;
+            succ = pred.next;
+        }else{
+            succ = tail;
+            for(int i = 0; i < size - index; i++) succ = succ.prev;
+            pred = succ.prev;
+        }
+        ++size;
+        ListNode toAdd = new ListNode(val);
+        toAdd.prev = pred;
+        toAdd.next = succ;
+        pred.next = toAdd;
+        succ.prev = toAdd;
+            
+        
+    }
+    
+    public void deleteAtIndex(int index) {
+    
+    if (index < 0 || index >= size) return;
+
+    ListNode pred, succ;
+    if (index < size - index) {
+      pred = head;
+      for(int i = 0; i < index; ++i) pred = pred.next;
+      succ = pred.next.next;
+    }
+    else {
+      succ = tail;
+      for (int i = 0; i < size - index - 1; ++i) succ = succ.prev;
+      pred = succ.prev.prev;
+    }
+
+    --size;
+    pred.next = succ;
+    succ.prev = pred;
+  }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+
+
+//===============Single Linked List====================
 // Time O(1) for addHead, O(N) for addTail O(K) for other ones.
 // Space O(1)
 
