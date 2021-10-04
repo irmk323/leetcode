8080b3170cceab11ed16dbe7a2308a7158e144fd
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-10-04 18:20:30 +0100

    Update 430_Flatten_a_Multilevel_Doubly_Linked_List.java

--- Java/430_Flatten_a_Multilevel_Doubly_Linked_List.java
+++ Java/430_Flatten_a_Multilevel_Doubly_Linked_List.java
@@ -8,27 +8,23 @@ class Node {
 };
 */
 
+// Time O(n) Space O(n)
+
 class Solution {
     public Node flatten(Node head) {
-        if( head == null) return head;
-	// Pointer
-        Node p = head; 
-        while( p!= null) {
-            /* CASE 1: if no child, proceed */
-            if( p.child == null ) {
+        if(head == null) return head;
+        Node p = head;
+        while( p != null) {
+            if(p.child == null){
                 p = p.next;
                 continue;
             }
-            /* CASE 2: got child, find the tail of the child and link it to p.next */
             Node temp = p.child;
-            // Find the tail of the child
-            while( temp.next != null ) 
+            while(temp.next != null)
                 temp = temp.next;
-            // Connect tail with p.next, if it is not null
-            temp.next = p.next;  
-            if( p.next != null )  p.next.prev = temp;
-            // Connect p with p.child, and remove p.child
-            p.next = p.child; 
+            temp.next= p.next;
+            if(p.next != null) p.next.prev = temp;
+            p.next = p.child;
             p.child.prev = p;
             p.child = null;
         }
