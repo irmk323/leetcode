a9b831b588caf791eb387eab914412383ce0467d
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-10-14 20:34:36 +0100

    Update 138_Copy_List_with_Random_Pointer.java

--- Java/138_Copy_List_with_Random_Pointer.java
+++ Java/138_Copy_List_with_Random_Pointer.java
@@ -12,51 +12,32 @@ class Node {
     }
 }
 */
-
+// Time O(N) Space O(1)
 class Solution {
-    public Node copyRandomList(Node head) {
-     Node iter = head, next;
-
-  // First round: make copy of each node,
-  // and link them together side-by-side in a single list.
-  while (iter != null) {
-    next = iter.next;
-
-    Node copy = new Node(iter.val);
-    iter.next = copy;
-    copy.next = next;
-
-    iter = next;
-  }
-
-  // Second round: assign random pointers for the copy nodes.
-  iter = head;
-  while (iter != null) {
-    if (iter.random != null) {
-      iter.next.random = iter.random.next;
-    }
-    iter = iter.next.next;
-  }
-
-  // Third round: restore the original list, and extract the copy list.
-  iter = head;
-  Node pseudoHead = new Node(0);
-  Node copy, copyIter = pseudoHead;
-
-  while (iter != null) {
-    next = iter.next.next;
-
-    // extract the copy
-    copy = iter.next;
-    copyIter.next = copy;
-    copyIter = copy;
-
-    // restore the original list
-    iter.next = next;
-
-    iter = next;
-  }
-
-  return pseudoHead.next;     
+     public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = new Node(cur.val, next, null);
+            cur = next;
+        }
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null)
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+        cur = head;
+        Node copyHead = head.next;
+        while (cur != null) {
+            Node next = cur.next.next;
+            Node copy = cur.next;
+            cur.next = next;
+            if (next != null)
+                copy.next = next.next;
+            cur = next;
+        }
+        return copyHead;
     }
 }
