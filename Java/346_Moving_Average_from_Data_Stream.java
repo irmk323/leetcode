07d8671bbedc35eafd109c2ffea5366e75c89237
irmk323 <31910138+irmk323@users.noreply.github.com>
2021-10-17 16:45:34 +0100

    Create 346_Moving_Average_from_Data_Stream.java

---
+++ Java/346_Moving_Average_from_Data_Stream.java
@@ -0,0 +1,29 @@
+// Time O(N) Space O(1)
+public class MovingAverage {
+    private int [] window;
+    private int n, insert;
+    private long sum;
+    
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        window = new int[size];
+        insert = 0;
+        sum = 0;
+    }
+    
+    public double next(int val) {
+        if (n < window.length)  n++;
+        sum -= window[insert];
+        sum += val;
+        window[insert] = val;
+        insert = (insert + 1) % window.length;
+        
+        return (double)sum / n;
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
