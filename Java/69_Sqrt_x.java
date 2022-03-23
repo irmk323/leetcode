744ab4c07b4f0e9bcb657ea95c8ea05fb01c3f3f
M.I <31910138+irmk323@users.noreply.github.com>
2022-03-23 16:25:09 +0000

    Create 69_Sqrt_x.java

---
+++ Java/69_Sqrt_x.java
@@ -0,0 +1,24 @@
+//Time complexity : O(logN)
+// Space complexity: O(1)
+
+class Solution {
+    public int mySqrt(int x) {
+        long start = 0;
+        long end = x;
+        
+        while(start + 1 < end ){
+            long mid = start + (end - start) / 2;
+            if(mid * mid == x){
+                return (int)mid;
+            }else if( mid * mid < x){
+                start = mid;
+            }else {
+                end = mid;
+            }
+        }
+        if(end * end == x) {
+            return (int)end;
+        }
+        return (int)start;
+    }
+}
