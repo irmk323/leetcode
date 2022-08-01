daae702400b3163a8a9b3880114fa99a40625394
irmk323 <irmk323@yahoo.co.jp>
2022-08-01 23:50:27 +0100

    optimizated

--- Python/2034_stock-price-fluctuation.py
+++ Python/2034_stock-price-fluctuation.py
@@ -5,40 +5,52 @@ class StockPrice:
         self.max_time = 0
         self.max_heap = []
         self.min_heap = []
+# 1:3
+# 2:5
 
     def update(self, timestamp: int, price: int) -> None:
-        self.timestamps[timestamp] = price
+        # O(1)
+        self.timestamps[timestamp] = price 
+        # O(1)
         self.max_time = max(self.max_time, timestamp)
         
         # refer the link for heapq
         # https://qiita.com/T_Wakasugi/items/dac6eb77a3cace54f95e
+        # O(log n) n : len(min_heap)
         heapq.heappush(self.min_heap, (price, timestamp))
         # If the element is taken out (pop) and the sign is reversed again (multiplied by -1), the maximum value can eventually be taken out.
+        # O(log n) n : len(max_heap)
         heapq.heappush(self.max_heap, (-price, timestamp))
         
 
     def current(self) -> int:
+        # O(1)
         return self.timestamps[self.max_time]
 
     def maximum(self) -> int:
-        cur_price, timestamp = heapq.heappop(self.max_heap)
+        # O(log n)
+        cur_price, timestamp = self.max_heap[0]
+        # O(n log n) log n on average 
+        # O(1) if we use .peek() average, 
         
         while -cur_price != self.timestamps[timestamp]:
-            cur_price, timestamp = heapq.heappop(self.max_heap)
+            heapq.heappop(self.max_heap)
+            cur_price, timestamp = self.max_heap[0]
+            
 
-        heapq.heappush(self.max_heap, (cur_price, timestamp))
+        # heapq.heappush(self.max_heap, (cur_price, timestamp))
         return -cur_price
 
     def minimum(self) -> int:
-        cur_price, timestamp = heapq.heappop(self.min_heap)
+        cur_price, timestamp = self.min_heap[0]
         
         while cur_price != self.timestamps[timestamp]:
-            cur_price, timestamp = heapq.heappop(self.min_heap)
-        heapq.heappush(self.min_heap, (cur_price, timestamp))
+            heapq.heappop(self.min_heap)
+            cur_price, timestamp = self.min_heap[0]
         return cur_price
         
 # Time
-# Update O(log K)
+# Update O(log n)
 # Current O(1)
 # Max k * log K
 # Min k * log K
