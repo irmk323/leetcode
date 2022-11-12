5d6f00c08333edf813738fc815e512447c1e82d2
irmk323 <irmk323@yahoo.co.jp>
2022-11-12 12:43:55 +0000

    improved 1631_path-with-minimum-effort.py

--- Python/1631_path-with-minimum-effort.py
+++ Python/1631_path-with-minimum-effort.py
@@ -1,21 +1,21 @@
-class Solution(object):
-    def minimumEffortPath(self, heights):
-        m, n = len(heights), len(heights[0])
-        effort = [[math.inf] * n for _ in range(m)]
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        col = len(heights[0])
+        row = len(heights)
+        effort = [[math.inf] * col for _ in range(row) ]
         effort[0][0] = 0
-        minHeap = [(0, 0, 0)] # distance, row, col
-        DIR = [0, 1, 0, -1, 0]
-
+        minHeap = [(0,0,0)]
+        DIR = [0,-1, 0, 1, 0]
+        
         while minHeap:
-            d, r, c = heappop(minHeap)
-            if d > effort[r][c]: continue 
-            if r == m - 1 and c == n - 1:
-                return d  # Reach to bottom right
-            
+            d,r,c = heappop(minHeap)
+            if c == col -1 and r == row -1:
+                return d
             for i in range(4):
                 nr, nc = r + DIR[i], c + DIR[i+1]
-                if 0 <= nr < m and 0 <= nc < n:
+                if 0 <= nr < row and 0 <= nc < col:
                     new_diff_to_take = max(d, abs(heights[nr][nc] - heights[r][c]))
                     if effort[nr][nc] > new_diff_to_take:
                         effort[nr][nc] = new_diff_to_take
                         heappush(minHeap, (effort[nr][nc], nr, nc))
+        
