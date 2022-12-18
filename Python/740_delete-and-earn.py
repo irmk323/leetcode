d8a13e063aad56bc643988d24ccf9f47934f8777
irmk323 <irmk323@yahoo.co.jp>
2022-12-18 23:42:21 +0000

    740. Delete and Earn

--- Python/740_delete-and-earn.py
+++ Python/740_delete-and-earn.py
@@ -1,18 +1,15 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         points = defaultdict(int)
-        max_number = 0
-        # Precompute how many points we gain from taking an element
+        max_num = 0
         for num in nums:
             points[num] += num
-            max_number = max(max_number, num)
+            max_num = max(max_num, num)
 
-        # Declare our array along with base cases
-        max_points = [0] * (max_number + 1)
+        max_points = [0] * (max_num + 1)  
         max_points[1] = points[1]
 
-        for num in range(2, len(max_points)):
-            # Apply recurrence relation
-            max_points[num] = max(max_points[num - 1], max_points[num - 2] + points[num])
-        
-        return max_points[max_number]
+        for i in range(2, len(max_points)):
+            max_points[i] = max(max_points[i-1],max_points[i-2]+points[i])
+
+        return max_points[max_num]
