02f16a561fa3aa5063a1cb74837c0b33ea973c8e
irmk323 <irmk323@gmail.com>
2023-09-30 20:51:25 +0100

    added comment

--- Java/2_Add_Two_Numbers.java
+++ Java/2_Add_Two_Numbers.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 
 /**
  * Definition for singly-linked list.
@@ -22,27 +12,29 @@
 // consize solution
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // everytime we consume node, 
+        // we need to care carryout and still nodes are there.
         int carry = 0;
-        ListNode p, dummy = new ListNode(0);
+        ListNode p,  dummy = new ListNode(0);
+        // p moves for take next, but dummy is pointing the beginning.
         p = dummy;
-        while(l1 != null || l2 != null || carry != 0){
-            if(l1 != null){
+        while(l1 != null || l2 !=null|| carry !=0){
+            if(l1!= null){
                 carry += l1.val;
                 l1 = l1.next;
             }
-            if(l2 != null){
+            if(l2!= null){
                 carry += l2.val;
                 l2 = l2.next;
             }
             p.next = new ListNode(carry%10);
-            carry /= 10;
             p = p.next;
+            carry /=10;
         }
-        return dummy.next;        
+        return dummy.next;
+        
     }
-}
-
- 
+} 
 
 // // Time O(n) SpaceO(n)
 // class Solution {
