76af79b493b0fc805b3653f05e8a133771877bc0
irmk323 <irmk323@gmail.com>
2024-02-10 15:18:00 +0000

    revised 138. Copy List with Random Pointer

--- Java/138_Copy_List_with_Random_Pointer.java
+++ Java/138_Copy_List_with_Random_Pointer.java
@@ -12,32 +12,48 @@ class Node {
     }
 }
 */
-// Time O(N) Space O(1)
+// Time O(N)
+// Space(1)
 class Solution {
-     public Node copyRandomList(Node head) {
-        if (head == null) return null;
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
         Node cur = head;
-        while (cur != null) {
+        // creating a copy node
+        // e.g.  
+        // A->B->C
+        // A->A'->B->B'->C->C'
+        while(cur != null){
             Node next = cur.next;
             cur.next = new Node(cur.val, next, null);
             cur = next;
         }
-        cur = head;
-        while (cur != null) {
-            if (cur.random != null)
+
+        cur = head; // reset the pointer to the head.
+        while(cur != null){
+            if(cur.random != null)
+
+                // A->A'->B->B'->C->C'
+                // This case setting A ramdom to A' random;
                 cur.next.random = cur.random.next;
+            // Otherwise just go to the next original node(not ' char )
             cur = cur.next.next;
         }
-        cur = head;
-        Node copyHead = head.next;
-        while (cur != null) {
-            Node next = cur.next.next;
-            Node copy = cur.next;
-            cur.next = next;
-            if (next != null)
-                copy.next = next.next;
-            cur = next;
+
+        cur = head; // reset the cur to the head again
+        Node copyHead = head.next; // means A'
+        while(cur != null){ 
+            // making 
+            // A->A'->B->B'->C->C'
+            // to
+            // A->B->C A'->B'->C'
+            Node originalNext = cur.next.next; // without ' 
+            Node copy = cur.next; // with '
+            cur.next = originalNext;
+            if(originalNext !=null){
+                copy.next = originalNext.next; // linking to ' chars
+            }
+            cur = originalNext;
         }
-        return copyHead;
+        return copyHead;      
     }
 }
