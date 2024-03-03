b4b245bdca3f207d15a387695ad0f4023932cfd7
irmk323 <irmk323@gmail.com>
2024-03-03 16:59:22 +0000

    optimized the right range

--- Java/2616_minimize-the-maximum-difference-of-pairs.java
+++ Java/2616_minimize-the-maximum-difference-of-pairs.java
@@ -14,7 +14,7 @@ class Solution {
     public int minimizeMax(int[] nums, int p) {
         Arrays.sort(nums);
         int  n = nums.length;
-        int left = 0, right = nums[n-1];
+        int left = 0, right = nums[n-1] - nums[0];
         while(left <= right){
             int mid = (left + right) / 2;
 
