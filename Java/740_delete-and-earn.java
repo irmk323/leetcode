012dfaa475ea3bd6d79f45811b1a426a64c46de3
irmk323 <irmk323@gmail.com>
2024-08-03 13:27:18 +0100

    refactored the names and to make it shorter code

--- Java/740_delete-and-earn.java
+++ Java/740_delete-and-earn.java
@@ -1,25 +1,24 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-       Map<Integer, Integer> freqMap = new HashMap<>();
-       for(int num : nums){
-           freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
-       }
-       List<Integer> list= new ArrayList(freqMap.keySet());
-       int[] keylist = list.stream().mapToInt(Integer::intValue).toArray();
-       Arrays.sort(keylist);
-       int earn1 = 0 , earn2 = 0;
-        for(int i = 0; i< keylist.length; i++ ){
-            int curEarn = keylist[i] * freqMap.get(keylist[i]);
-            if(i > 0 && keylist[i] == keylist[i-1] +1 ){
-                int temp = earn2;
-                earn2 = Math.max(curEarn + earn1, earn2 );
-                earn1 = temp;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num: nums){
+            freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
+        }
+        List<Integer> list = new ArrayList<>(freqMap.keySet());
+        int[] keyList = list.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(keyList);
+        int oneLeft = 0; int twoLeft = 0;
+        for(int i =0; i <keyList.length; i++ ){
+            int curVal = keyList[i] * freqMap.get(keyList[i]);
+            int temp = oneLeft;
+            if(i > 0 && keyList[i] == keyList[i-1]+1){
+                oneLeft = Math.max(curVal + twoLeft, oneLeft);
+                
             }else{
-                 int temp = earn2;
-                 earn2 = curEarn + earn2;
-                 earn1 = temp;
+                oneLeft+= curVal;
             }
-        } 
-        return earn2;    
+            twoLeft = temp;
+        }
+        return oneLeft;
     }
 }
